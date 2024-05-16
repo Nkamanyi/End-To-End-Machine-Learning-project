9f50f9b13c412864a1b00dfb72dcfad10149b5dd
Nkamanyi <Chatutie.Nkamanyimartin@tuni.fi>
2024-05-16 07:04:15 +0300

    modify logger.py

--- src/logger.py
+++ src/logger.py
@@ -2,16 +2,22 @@ import logging
 import os
 from datetime import datetime
 
-LOG_FILE=f"{datetime.now().strftime('%m_%d_%Y_%H_%M_%S')}.log"
-logs_path=os.path.join(os.getcwd(),"logs",LOG_FILE)
-os.makedirs(logs_path,exist_ok=True)
+# Directory for logs
+log_dir_name = "logs"
+log_dir_path = os.path.join(os.getcwd(), log_dir_name)
+# Ensure the directory exists
+os.makedirs(log_dir_path, exist_ok=True)
 
-LOG_FILE_PATH=os.path.join(logs_path,LOG_FILE)
+# Log file with a timestamp
+log_file_name = f"{datetime.now().strftime('%Y_%m_%d_%H_%M_%S')}.log"
+log_file_path = os.path.join(log_dir_path, log_file_name)
 
+# Basic configuration for logging
 logging.basicConfig(
-    filename=LOG_FILE_PATH,
-    format="[ %(asctime)s ] %(lineno)d %(name)s - %(levelname)s - %(message)s",
-    level=logging.INFO,
-
-
+    filename=log_file_path,
+    format="[%(asctime)s] %(lineno)d %(name)s - %(levelname)s - %(message)s",
+    level=logging.INFO
 )
+
+if __name__ == "__main__":
+    logging.info("Logging has started")
